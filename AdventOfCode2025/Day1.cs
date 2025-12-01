8e8379df9609cfb09001bc5d523a702c89474073
msklein <klein_m@hotmail.com>
2025-12-01 19:07:45 +0200

    day 1 part 1 and 2

---
+++ AdventOfCode2025/Day1.cs
@@ -0,0 +1,122 @@
+﻿namespace AdventOfCode2025
+{
+    public class Day1
+    {
+        public int Part1()
+        {
+            var directory = AppContext.BaseDirectory;
+            var path = System.IO.Path.Combine(directory, "Inputs", "Input_D1.txt");
+            var outputLines = new InputFile().ReadInputFile(path);
+            var nodeList = new LinkedList<int>();
+            int startingPosition = 50;
+            int passCode = 0;
+            int currentPosition = startingPosition;
+
+            for (int i = 0; i < 100; i++)
+            {
+                nodeList.AddLast(i);
+            }
+
+            var currentNode = nodeList.Find(startingPosition);
+
+            foreach (var line in outputLines)
+            {
+                var direction = line.Substring(0, 1);
+                var value = int.Parse(line.Substring(1, line.Length - 1));
+
+                if (direction == "L")
+                {
+                    for (int i = 0; i < value; i++)
+                    {
+                        currentNode = currentNode.Previous ?? nodeList.Last;
+                    }
+                }
+                else if (direction == "R")
+                {
+                    for (int i = 0; i < value; i++)
+                    {
+                        currentNode = currentNode.Next ?? nodeList.First;
+                    }
+                }
+
+                if (currentNode.Value == 0)
+                {
+                    passCode++;
+                }
+            }
+
+            return passCode;
+        }
+
+        public int Part2()
+        {
+            var directory = AppContext.BaseDirectory;
+            var path = System.IO.Path.Combine(directory, "Inputs", "Input_D1.txt");
+            var outputLines = new InputFile().ReadInputFile(path);
+            var nodeList = new LinkedList<int>();
+            int startingPosition = 50;
+            int passCode = 0;
+            int currentPosition = startingPosition;
+
+            for (int i = 0; i < 100; i++)
+            {
+                nodeList.AddLast(i);
+            }
+
+            var currentNode = nodeList.Find(startingPosition);
+
+            foreach (var line in outputLines)
+            {
+                var direction = line.Substring(0, 1);
+                var value = int.Parse(line.Substring(1, line.Length - 1));
+
+                if (direction == "L")
+                {
+                    for (int i = 0; i < value; i++)
+                    {
+                        if (currentNode.Value == 0)
+                        {
+                            passCode++;
+                        }
+
+                        if (currentNode.Previous == null)
+                        {
+                            currentNode = nodeList.Last;
+                        }
+                        else
+                        {
+                            currentNode = currentNode.Previous;
+                        }
+                    }
+                }
+                else if (direction == "R")
+                {
+                    for (int i = 0; i < value; i++)
+                    {
+                        if (currentNode.Value == 0)
+                        {
+                            passCode++;
+                        }
+
+                        if (currentNode.Next == null)
+                        {
+                            currentNode = nodeList.First;
+                        }
+                        else
+                        {
+                            currentNode = currentNode.Next;
+                        }
+                    }
+                }
+            }
+
+            //Check if it ends on 0
+            if (currentNode.Value == 0)
+            {
+                passCode++;
+            }
+
+            return passCode;
+        }
+    }
+}
--- AdventOfCode2025/Program.cs
+++ AdventOfCode2025/Program.cs
@@ -1,8 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-using AdventOfCode2025.Inputs;
-
-Console.WriteLine("Hello, World!");
+﻿using AdventOfCode2025;
 
 Day1 day1 = new Day1();
 var resultPart1 = day1.Part1();
 Console.WriteLine($"Day 1, Part 1 Result: {resultPart1}");
+var resultPart2 = day1.Part2();
+Console.WriteLine($"Day 1, Part 2 Result: {resultPart2}");
