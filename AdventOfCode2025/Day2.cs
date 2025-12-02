53d22671a092798555bab09c851d27822de5b3cf
msklein <klein_m@hotmail.com>
2025-12-02 21:32:09 +0200

    day 2 part 1 and 2

---
+++ AdventOfCode2025/Day2.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode2025
+{
+    public class Day2
+    {
+        public long Part1()
+        {
+            long result = 0;
+            var outputLines = new InputFile().ReadInputFile("Input_D2.txt");
+            var idRanges = outputLines.First().Split(',').Select(range =>
+            {
+                var bounds = range.Split('-').Select(long.Parse).ToArray();
+                return (Min: bounds[0], Max: bounds[1]);
+            }).ToArray();
+
+            Regex pattern = new Regex(@"^(\d+)\1$");
+
+            foreach (var idRange in idRanges)
+            {
+                IEnumerable<long> range = CreateLongRange(idRange.Min, idRange.Max - idRange.Min +1);
+
+                foreach (var number in range)
+                {
+                    var match = pattern.Match(number.ToString());
+
+                    if (match.Success)
+                    {
+                        result += number;
+                    }
+                };
+            }
+
+            return result;
+        }
+
+        public long Part2()
+        {
+            long result = 0;
+            var outputLines = new InputFile().ReadInputFile("Input_D2.txt");
+            var idRanges = outputLines.First().Split(',').Select(range =>
+            {
+                var bounds = range.Split('-').Select(long.Parse).ToArray();
+                return (Min: bounds[0], Max: bounds[1]);
+            }).ToArray();
+
+            Regex pattern = new Regex(@"^(\d+)\1+$");
+
+            foreach (var idRange in idRanges)
+            {
+                IEnumerable<long> range = CreateLongRange(idRange.Min, idRange.Max - idRange.Min + 1);
+
+                foreach (var number in range)
+                {
+                    var match = pattern.Match(number.ToString());
+
+                    if (match.Success)
+                    {
+                        result += number;
+                    }
+                }
+                ;
+            }
+
+            return result;
+        }
+
+        private static IEnumerable<long> CreateLongRange(long start, long count)
+        {
+            long limit = start + count;
+            for (long i = start; i < limit; i++)
+            {
+                yield return i;
+            }
+        }
+    }
+}
--- AdventOfCode2025/Program.cs
+++ AdventOfCode2025/Program.cs
@@ -1,7 +1,15 @@
 ﻿using AdventOfCode2025;
 
-Day1 day1 = new Day1();
-var resultPart1 = day1.Part1();
-Console.WriteLine($"Day 1, Part 1 Result: {resultPart1}");
-var resultPart2 = day1.Part2();
+//Day1 day1 = new Day1();
+//var resultPart1 = day1.Part1();
+//Console.WriteLine($"Day 1, Part 1 Result: {resultPart1}");
+//var resultPart2 = day1.Part2();
+//Console.WriteLine($"Day 1, Part 2 Result: {resultPart2}");
+
+/////////////////////////////////////////////////////////////
+
+Day2 day2 = new Day2();
+var resultPart1 = day2.Part1();
+Console.WriteLine($"Day 2, Part 1 Result: {resultPart1}");
+var resultPart2 = day2.Part2();
 Console.WriteLine($"Day 1, Part 2 Result: {resultPart2}");
