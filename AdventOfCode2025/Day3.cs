93e13684d5d7711248a91c88baf12a0aad729bdf
msklein <klein_m@hotmail.com>
2025-12-03 22:27:44 +0200

    day 3 part 1

---
+++ AdventOfCode2025/Day3.cs
@@ -0,0 +1,53 @@
+﻿namespace AdventOfCode2025
+{
+    public class Day3
+    {
+        public long Part1()
+        {
+            long result = 0;
+            var outputLines = new InputFile().ReadInputFile("Input_D3.txt");
+
+            foreach (var line in outputLines)
+            {
+                var digits = line.ToCharArray();
+
+                var subLine = line.Substring(0, line.Length - 1);
+                var firstDigit = subLine.Max(c => c.ToString());
+                var positionOfMax = Array.IndexOf(digits, firstDigit[0]);
+
+                var newDigits = digits.Where((c, index) => index > positionOfMax).ToArray();
+                var secondDigit = newDigits.Max(c => c.ToString());
+
+                result += long.Parse(firstDigit + secondDigit);
+            }
+
+            return result;
+        }
+        public long Part2()
+        {
+            long result = 0;
+            var outputLines = new InputFile().ReadInputFile("Input_D3.txt");
+
+            foreach (var line in outputLines)
+            {
+                var digits = line.ToCharArray();
+
+                var subLine = line.Substring(0, line.Length - 1);
+                var firstDigit = subLine.Max(c => c.ToString());
+                var positionOfMax = Array.IndexOf(digits, firstDigit[0]);
+
+                var newDigits = digits.Where((c, index) => index > positionOfMax).ToArray();
+                var secondDigit = newDigits.Max(c => c.ToString());
+                
+                result += long.Parse(firstDigit + secondDigit);
+
+                for (int i = 0; i < 12; i++)
+                {
+
+                }
+            }
+
+            return result;
+        }
+    }
+}
--- AdventOfCode2025/Program.cs
+++ AdventOfCode2025/Program.cs
@@ -8,8 +8,16 @@
 
 /////////////////////////////////////////////////////////////
 
-Day2 day2 = new Day2();
-var resultPart1 = day2.Part1();
-Console.WriteLine($"Day 2, Part 1 Result: {resultPart1}");
-var resultPart2 = day2.Part2();
-Console.WriteLine($"Day 1, Part 2 Result: {resultPart2}");
+//Day2 day2 = new Day2();
+//var resultPart1 = day2.Part1();
+//Console.WriteLine($"Day 2, Part 1 Result: {resultPart1}");
+//var resultPart2 = day2.Part2();
+//Console.WriteLine($"Day 2, Part 2 Result: {resultPart2}");
+
+/////////////////////////////////////////////////////////////
+
+Day3 day3 = new Day3();
+var resultPart1 = day3.Part1();
+Console.WriteLine($"Day 3, Part 1 Result: {resultPart1}");
+//var resultPart2 = day3.Part2();
+//Console.WriteLine($"Day 3, Part 2 Result: {resultPart2}");
